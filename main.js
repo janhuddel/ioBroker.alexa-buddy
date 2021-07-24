4debc467691f53d7b92202667e543fe47278aae1
Jan Rohwer <jan.rohwer@gmail.com>
2021-07-24 21:57:49 +0200

    first working version

--- main.js
+++ main.js
@@ -8,8 +8,7 @@
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const io = require("socket.io-client");
 
 /**
  * The adapter instance
@@ -18,137 +17,90 @@ const utils = require("@iobroker/adapter-core");
 let adapter;
 
 /**
+ * Websocket-Client
+ */
+let socket = null;
+
+/**
  * Starts the adapter instance
  * @param {Partial<utils.AdapterOptions>} [options]
  */
 function startAdapter(options) {
-    // Create the adapter and define its methods
-    return adapter = utils.adapter(Object.assign({}, options, {
-        name: "alexa-buddy",
+  return (adapter = utils.adapter(
+    Object.assign({}, options, {
+      name: "alexa-buddy",
 
-        // The ready callback is called when databases are connected and adapter received configuration.
-        // start here!
-        ready: main, // Main method defined below for readability
+      // The ready callback is called when databases are connected and adapter received configuration.
+      ready: main, // Main method defined below for readability
 
-        // is called when adapter shuts down - callback has to be called under any circumstances!
-        unload: (callback) => {
-            try {
-                // Here you must clear all timeouts or intervals that may still be active
-                // clearTimeout(timeout1);
-                // clearTimeout(timeout2);
-                // ...
-                // clearInterval(interval1);
-
-                callback();
-            } catch (e) {
-                callback();
-            }
-        },
-
-        // If you need to react to object changes, uncomment the following method.
-        // You also need to subscribe to the objects with `adapter.subscribeObjects`, similar to `adapter.subscribeStates`.
-        // objectChange: (id, obj) => {
-        //     if (obj) {
-        //         // The object was changed
-        //         adapter.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-        //     } else {
-        //         // The object was deleted
-        //         adapter.log.info(`object ${id} deleted`);
-        //     }
-        // },
-
-        // is called if a subscribed state changes
-        stateChange: (id, state) => {
-            if (state) {
-                // The state was changed
-                adapter.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-            } else {
-                // The state was deleted
-                adapter.log.info(`state ${id} deleted`);
-            }
-        },
-
-        // If you need to accept messages in your adapter, uncomment the following block.
-        // /**
-        //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-        //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-        //  */
-        // message: (obj) => {
-        //     if (typeof obj === "object" && obj.message) {
-        //         if (obj.command === "send") {
-        //             // e.g. send email or pushover or whatever
-        //             adapter.log.info("send command");
-
-        //             // Send response in callback if required
-        //             if (obj.callback) adapter.sendTo(obj.from, obj.command, "Message received", obj.callback);
-        //         }
-        //     }
-        // },
-    }));
+      // is called when adapter shuts down - callback has to be called under any circumstances!
+      unload: (callback) => {
+        if (socket) {
+          socket.disconnect();
+          socket = null;
+        }
+        callback();
+      },
+    })
+  ));
 }
 
 async function main() {
+  // Reset the connection indicator during startup
+  await adapter.setStateAsync("info.connection", false, true);
 
-    // Reset the connection indicator during startup
-    await this.setStateAsync("info.connection", false, true);
+  const apikey = adapter.config.apikey;
+  const server = adapter.config.server;
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info("config option1: " + adapter.config.option1);
-    adapter.log.info("config option2: " + adapter.config.option2);
-
-    /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-    */
-    await adapter.setObjectNotExistsAsync("testVariable", {
-        type: "state",
-        common: {
-            name: "testVariable",
-            type: "boolean",
-            role: "indicator",
-            read: true,
-            write: true,
-        },
-        native: {},
+  if (!socket) {
+    socket = io(server, {
+      transports: ["websocket"],
+      query: `apikey=${apikey}`,
     });
+  } else {
+    console.log("websocket already connected");
+  }
 
-    // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-    adapter.subscribeStates("testVariable");
-    // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-    // adapter.subscribeStates("lights.*");
-    // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-    // adapter.subscribeStates("*");
+  socket.on("connect", async () => {
+    adapter.log.info(`websocket connected with ${server}`);
+    await adapter.setStateAsync("info.connection", true, true);
+  });
 
-    /*
-        setState examples
-        you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-    */
-    // the variable testVariable is set to true as command (ack=false)
-    await adapter.setStateAsync("testVariable", true);
+  socket.on("disconnect", async (reason) => {
+    adapter.log.info("websocket disconnected", reason);
+    await adapter.setStateAsync("info.connection", false, true);
+  });
 
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    await adapter.setStateAsync("testVariable", { val: true, ack: true });
+  socket.on("connect_error", async (error) => {
+    let errorMessage;
+    if (error.description && error.description.message) {
+      errorMessage = error.description.message;
+    } else {
+      errorMessage = error.message;
+    }
+    adapter.log.error(`websocket-error: ${errorMessage}`);
+    await adapter.setStateAsync("info.connection", false, true);
+  });
 
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    await adapter.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+  socket.on("setState", async (state, callback) => {
+    console.log(`set value of ${state.datapoint} to ${state.value}...`);
+    await adapter.setForeignStateAsync(state.datapoint, state.value);
 
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword("admin", "iobroker", (res) => {
-        adapter.log.info("check user admin pw iobroker: " + res);
-    });
+    callback(state.value);
+  });
 
-    adapter.checkGroup("admin", "admin", (res) => {
-        adapter.log.info("check group user admin group admin: " + res);
-    });
+  socket.on("getState", async (datapoint, callback) => {
+    console.log(`reading state of ${datapoint}...`);
+    const result = await adapter.getForeignStateAsync(datapoint);
+
+    callback(result);
+  });
 }
 
 if (require.main !== module) {
-    // Export startAdapter in compact mode
-    module.exports = startAdapter;
+  // Export startAdapter in compact mode
+  module.exports = startAdapter;
 } else {
-    // otherwise start the instance directly
-    startAdapter();
+  // otherwise start the instance directly
+  startAdapter();
 }
